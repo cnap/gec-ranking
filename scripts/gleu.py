6a03e475a9ceebfb18cfce4dd367912d94699618
Courtney Napoles <cdnapoles@gmail.com>
2016-03-25 12:46:10 -0400

    added add-one smoothing for sentence-level calculations

--- scripts/gleu.py
+++ scripts/gleu.py
@@ -27,12 +27,12 @@ class GLEU :
         self.hlen = len(hypothesis)
         self.this_h_ngrams = [ self.get_ngram_counts(hypothesis,n)
                                for n in range(1,self.order+1) ]
-        
+
     def load_sources(self,spath) :
         self.all_s_ngrams = [ [ self.get_ngram_counts(line.split(),n)
                                 for n in range(1,self.order+1) ]
                               for line in open(spath) ]
-        
+
     def load_references(self,rpaths) :
         self.refs = [ [] for i in range(len(self.all_s_ngrams)) ]
         self.rlens = [ [] for i in range(len(self.all_s_ngrams)) ]
@@ -43,31 +43,30 @@ class GLEU :
 
         # count number of references each n-gram appear sin
         self.all_rngrams_freq = [ Counter() for i in range(self.order) ]
-        
+
         self.all_r_ngrams = [ ]
         for refset in self.refs :
             all_ngrams = []
             self.all_r_ngrams.append(all_ngrams)
-            
+
             for n in range(1,self.order+1) :
                 ngrams = self.get_ngram_counts(refset[0],n)
                 all_ngrams.append(ngrams)
-                
+
                 for k in ngrams.keys() :
                     self.all_rngrams_freq[n-1][k]+=1
-                
+
                 for ref in refset[1:] :
                     new_ngrams = self.get_ngram_counts(ref,n)
                     for nn in new_ngrams.elements() :
                         if new_ngrams[nn] > ngrams.get(nn,0) :
                             ngrams[nn] = new_ngrams[nn]
 
-        
     def get_ngram_counts(self,sentence,n) :
         return Counter([tuple(sentence[i:i+n])
                         for i in xrange(len(sentence)+1-n)])
 
-    # returns ngrams in a but not in b    
+    # returns ngrams in a but not in b
     def get_ngram_diff(self,a,b) :
         diff = Counter(a)
         for k in (set(a) & set(b)) :
@@ -76,11 +75,11 @@ class GLEU :
 
     def normalization(self,ngram,n) :
         return 1.0*self.all_rngrams_freq[n-1][ngram]/len(self.rlens[0])
-    
+
     # Collect BLEU-relevant statistics for a single hypothesis/reference pair.
     # Return value is a generator yielding:
     # (c, r, numerator1, denominator1, ... numerator4, denominator4)
-    # Summing the columns across calls to this function on an entire corpus 
+    # Summing the columns across calls to this function on an entire corpus
     # will produce a vector of statistics that can be used to compute GLEU
     def gleu_stats(self,i,version=1,r_ind=None):
 
@@ -95,7 +94,7 @@ class GLEU :
           for r in self.rlens[i][1:] :
               if abs(r - hlen) < abs(rlen - hlen) :
                   rlen = r
-              
+
       yield rlen
       yield hlen
 
@@ -117,15 +116,17 @@ class GLEU :
                                            self.normalization(k,n)])
 
         yield max([ sum( (h_ngrams & r_ngrams).values() ) - \
-                    sum( (h_ngrams & s_ngram_diff).values() ), 0 ])      
-        
+                    sum( (h_ngrams & s_ngram_diff).values() ), 0 ])
+
         yield max([hlen+1-n, 0])
 
     # Compute GLEU from collected statistics obtained by call(s) to gleu_stats
-    def gleu(self,stats):
-       if len(filter(lambda x: x==0, stats)) > 0:
-         return 0
-       (c, r) = stats[:2]
-       log_gleu_prec = sum([math.log(float(x)/y)
-                            for x,y in zip(stats[2::2],stats[3::2])]) / 4
-       return math.exp(min([0, 1-float(r)/c]) + log_gleu_prec)
+    def gleu(self,stats,smooth=False):
+        if smooth :
+            stats = [ s if s != 0 else 1 for s in stats ]
+        if len(filter(lambda x: x==0, stats)) > 0:
+            return 0
+        (c, r) = stats[:2]
+        log_gleu_prec = sum([math.log(float(x)/y)
+                             for x,y in zip(stats[2::2],stats[3::2])]) / 4
+        return math.exp(min([0, 1-float(r)/c]) + log_gleu_prec)
